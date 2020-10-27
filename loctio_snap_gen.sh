4c54fc9a7d69ad8a47ad010c4a03e96a681baacd
Marios Karagiannopoulos from Jenkins <marios.karagiannopoulos@loctio.com>
2020-10-27 19:57:28 +0200

    Capturing 200ms snapshots

--- loctio_snap_gen.sh
+++ loctio_snap_gen.sh
@@ -12,19 +12,20 @@ do
 	then
 		#NOW=$(date -u '+%Y%m%d %H:%M:%S' -d '- 10 minutes')
 		NOW=$(date -u '+%Y%m%d %H:%M:%S')
-		#echo $NOW
+		echo $NOW
 		
 		DATE=$(date -u +'%Y/%m/%d,%H:%M:%S' -d "$NOW")
-		#echo $DATE
+		echo $DATE
 		
 		REF_TIME=$(date -u +'%Y%m%d%H%M%S' -d "$NOW")
-		#echo $REF_TIME
+		echo $REF_TIME
 		
 		#echo $LOCTIO_API_HOST
 		URL="http://$LOCTIO_API_HOST:50505/api/rinex?provider=nasa&ref_pos0=$LAT&ref_pos1=$LON&ref_pos2=$ALT&ref_time=$REF_TIME&raw=1"
-		curl -X GET $URL > rinex.txt				
+		curl -X GET $URL > rinex.txt
 		head -1 rinex.txt
-		GPS_SDR_SIM_CMD="./gps-sdr-sim -s 2800000 -b 1 -d 1 -e rinex.txt -t $DATE -T $DATE -l $GROUND_TRUTH -o $SNAPSHOT_SAMPLE_PATH"
+		GPS_SDR_SIM_CMD="./gps-sdr-sim -s 2800000 -b 1 -d 0.2 -e rinex.txt -t $DATE -l $GROUND_TRUTH -o $SNAPSHOT_SAMPLE_PATH"
+		echo $GPS_SDR_SIM_CMD
 		$($GPS_SDR_SIM_CMD)
 	else
 		ls -al $SNAPSHOT_SAMPLE_PATH
